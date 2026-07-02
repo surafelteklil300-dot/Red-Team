9c69f25f14c83673655e49af507be62e5488a020
surafelteklil300-dot <surafelteklil300@gmail.com>
2026-07-02 13:01:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -898,7 +898,7 @@ def main():
         socketio.run(
             app,
             host='0.0.0.0',
-            port=5000,
+            port=int(os.environ.get("PORT", 5000))
             debug=config.debug,
             allow_unsafe_werkzeug=True,
             use_reloader=False
